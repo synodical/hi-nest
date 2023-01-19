220a7cd894cf6fcd6549d54cada9df9354c044fd
이예은 <lena0323park@naver.com>
2023-01-19 11:32:41 +0900

    movies module 분리

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
+import { MoviesModule } from './movies/movies.module';
+import { AppController } from './app.controller';
 
-@Module({ // 데코레이터 -> 클래스에 함수 기능을 추가할 수 있음
+@Module({
+  // 데코레이터 -> 클래스에 함수 기능을 추가할 수 있음
   // 클래스 위의 함수, 클래스를 위해 움직임
-  imports: [],
-  controllers: [MoviesController],
-  providers: [MoviesService],
+  imports: [MoviesModule],
+  controllers: [AppController],
+  providers: [],
 })
 export class AppModule {}
